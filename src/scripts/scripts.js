30dd1c0e873a196444c785f1dd8d4d0fd1cfe89a
Dominik Kubis <kubis.dominik1@gmail.com>
2016-11-16 22:54:04 +0100

    [TASK-1] - create fetch polyfill

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,50 @@
+function Fetch(url, userSuccessCallback, userFailureHandler) {
+
+    if (arguments.length != 3) {
+        throw new Error('Fetch przyjmuje 3 argumenty: URL, success callback, failure callback');
+    }
+
+    if (typeof arguments[1] !== 'function') {
+        throw new Error('Success callback musi być funckcją');
+    }
+
+    if (typeof arguments[2] !== 'function') {
+        throw new Error('Failure callback musi być funkcją');
+    }
+
+    if (!(this instanceof Fetch)) {
+        return new Fetch(url, userSuccessCallback, userFailureHandler);
+    }
+
+
+    this._xhr = new XMLHttpRequest();
+    this._successHandler = userSuccessCallback;
+    this._failureHandler = userFailureHandler;
+
+    this._xhr.open('GET', url);
+    this._asignEvents();
+    this._xhr.send(null);
+}
+
+Fetch.prototype._handleResponse = function () {
+    if (this._xhr.readyState === 4 && this._xhr.status >= 200  && this._xhr.status < 400){
+        this._successHandler(this._xhr.response);
+    } else if (this._xhr.readyState === 4 && this._xhr.status >= 400) {
+        this._failureHandler(this._xhr);
+    }
+};
+
+Fetch.prototype._handleError = function () {
+    this._failureHandler(this._xhr);
+};
+
+Fetch.prototype._asignEvents = function () {
+    this._xhr.addEventListener('readystatechange', this._handleResponse.bind(this), false);
+    this._xhr.addEventListener('abort', this._handleError.bind(this), false);
+    this._xhr.addEventListener('error', this._handleError.bind(this), false);
+    this._xhr.addEventListener('timeout', this._handleError.bind(this), false);
+};
+
 var AjaxAPP = (function ($, Handlebars) {
 
     var textTemplate = $("#li-template").html();
@@ -17,26 +64,32 @@ var AjaxAPP = (function ($, Handlebars) {
     function getData(e) {
         e.preventDefault();
 
-        $.getJSON('https://jsonplaceholder.typicode.com/users', function (data) {
-            addElement(data)
-        })
-        .fail(function () {
-            renderErrorMessage("Ups! Stało się coś złego, nie udało się pobrać danych...");
-        });
+
+        Fetch(
+            'https://jsonplaceholder.typicode.com/users',
+            function (data) {
+                addElement(data);
+                console.log('Sukces!');
+            },
+            function (xhr) {
+                console.log(xhr);
+                console.log('Błąd!')
+            }
+        );
 
         $button.prop('disabled', true);
     }
 
     function addElement(data) {
 
-        if (data.length < 0) {
+        if (data.length == 0) {
             renderErrorMessage("Ups! Twoje dane gdzieś zniknęły...");
             return -1;
         }
 
         var df = document.createDocumentFragment();
 
-        $.each(data, function (i, el) {
+        $.each(JSON.parse(data), function (i, el) {
             $(df).append(compiledTemplate(el));
         });
 
